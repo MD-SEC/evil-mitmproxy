afff5fa99c2baca666411aa2596dfb0b1e443b2c
MD-SEC <MD_SEC@163.com>
2023-09-03 07:57:48 +0800

    jeecg-boot被动漏洞探测

---
+++ jeecg-boot.py
@@ -0,0 +1,31 @@
+import mitmproxy.http
+import re
+import csv
+
+class Counter:
+    
+    def __init__(self):
+        self.num = 0
+
+    def response(self, flow: mitmproxy.http.HTTPFlow):
+        text=flow.response.get_content()
+        if "VUE_APP_API" in str(text):                        #指纹识别，确定jeecg-boot站点
+            index=str(text).find("VUE_APP_API_BASE_URL") 
+            pattern=r"VUE_APP_API_BASE_URL:\"(.+)\","         #正则匹配，提取真实jeecg-boot接口
+            match=re.search(pattern=pattern,string=str(text)[index:index+100])
+            if match:
+                api = match.group(1)                          #未授权访问
+                sql1=api+"/jmreport/queryFieldBySql"          #sql注入rce1
+                sql2=api+"/jmreport/testConnection"           #sql注入rce2
+                print(api)
+                with open("demo1.csv", "wt") as csvfile:
+                    writer = csv.writer(csvfile)              #拿到接口后写入文件
+                    writer.writerow([api])
+                    writer.writerow([sql1])
+                    writer.writerow([sql2])
+                    
+                
+                
+addons = [
+    Counter()
+]
